3463844c6807bea1d18a4abf6cc919f1fe05908d
WalterJin2 <walterjin2@gmail.com>
2023-02-25 20:36:32 +1100

    Add picturesApiSlice to request pictures from server

---
+++ src/features/pictures/picturesApiSlice.js
@@ -0,0 +1,40 @@
+import { apiSlice } from '../../app/api/apiSlice'
+
+export const picturesApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getPicturesPublic: builder.query({
+      query: (input) => ({
+        url: `/picturePublic`,
+        method: 'GET',
+        mode: 'cors',
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError
+        },
+      }),
+      transformResponse: (responseData) => {
+        return responseData.data.items
+      },
+      providesTags: ['getPicturesPublic'],
+      invalidatesTags: ['getPicturesPublic'],
+    }),
+    getPicturesPrivate: builder.query({
+      query: (input) => ({
+        url: `/picturePrivate`,
+        method: 'POST',
+        body: { input },
+        mode: 'cors',
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError
+        },
+      }),
+      transformResponse: (responseData) => {
+        return responseData.data.items
+      },
+      providesTags: ['getPicturs'],
+      invalidatesTags: ['getPicturs'],
+    }),
+  }),
+})
+
+export const { useGetPicturesPublicQuery, useGetPicturesPrivateQuery } =
+  picturesApiSlice

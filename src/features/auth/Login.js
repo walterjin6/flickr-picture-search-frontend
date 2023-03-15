a939a52420c562b9fb30937673150cd172018eef
WalterJin2 <walterjin2@gmail.com>
2023-03-16 04:48:01 +1100

    a

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -152,7 +152,7 @@ const Login = () => {
             onSubmit={handleSubmit}
           >
             <label htmlFor='username' className=' font-bold py-2'>
-              Username: [Input 'guest' for guests]
+              Username: [Input 'Guest' for guests]
             </label>
             <input
               className=' border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 '

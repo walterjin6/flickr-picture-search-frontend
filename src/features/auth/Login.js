8b91cbbcacde97d2e90258fc832016bf3dacb312
WalterJin2 <walterjin2@gmail.com>
2023-03-16 03:51:19 +1100

    a

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -145,15 +145,6 @@ const Login = () => {
               </Link>
             </div>
           </header>
-          <button
-            type='submit'
-            className='w-full inline-block px-7 py-3 bg-yellow-600 text-white font-bold text-xl leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
-            data-mdb-ripple='true'
-            data-mdb-ripple-color='light'
-            onClick={handleGuestPortal}
-          >
-            Guest Portal - Login Free
-          </button>
           <p ref={errRef} className={errClass} aria-live='assertive'>
             {errMsg}
           </p>
@@ -214,6 +205,16 @@ const Login = () => {
                 Trust This Device
               </label>
             </div>
+            <span className='py-4 font-bold'>Not registered? Use guest portal below for login-free</span>
+            <button
+              type='submit'
+              className='w-full inline-block px-7 py-3 bg-yellow-600 text-white font-bold text-xl leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+              data-mdb-ripple='true'
+              data-mdb-ripple-color='light'
+              onClick={handleGuestPortal}
+            >
+              Guest Portal - Login Free
+            </button>
           </form>
         </main>
       </section>

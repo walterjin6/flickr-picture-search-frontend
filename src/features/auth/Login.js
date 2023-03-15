6a8f114fe8f9ee02f3a60fd76fa61d638553e6b3
WalterJin2 <walterjin2@gmail.com>
2023-03-16 05:38:12 +1100

    a

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -203,7 +203,7 @@ const Login = () => {
                 Trust This Device
               </label>
             </div>
-            <span className='py-4 font-bold'>Not registered? Click guest portal below for login-free</span>
+            <p className='py-8 font-bold text-center'>Not registered? Click guest portal below for login-free</p>
             <button
               type='submit'
               className='w-full inline-block px-7 py-3 bg-yellow-600 text-white font-bold text-xl leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
